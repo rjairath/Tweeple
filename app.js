c320d2bdaf936f256844eb6d41d6261f09558987
rjairath <rishabhjairath@gmail.com>
2019-06-01 15:47:17 +0530

    follow and like bot

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
 
 const config = require('./config');
 const Twitter = require('twitter');
+const argv = require('yargs').argv;
 
 const T = new Twitter(config);
 
+const likeParam = (argv.l)?`#${argv.l}`:null;
+const followParam = (argv.f)?`#${argv.f}`:null;
+
 const params = {
-    q: '#india',
+    q: ((likeParam)?likeParam:followParam),
     count: 2,
     result_type: 'recent',
     lang: 'en'
 }
-
+// console.log(params);
 T.get('search/tweets', params, (err, data, response)=>{
     if(!err){
-        //Favourite all tweets here
-        for(let i=0; i<data.statuses.length; i++){
-            let id = {id: data.statuses[i].id_str};
-            T.post('favorites/create', id, (err, response)=>{
-                if(!err){
-                    let userName = response.user.screen_name;
-                    let id = response.id_str;
-                    let url = `https://twitter.com/${userName}/status/${id}`;
-
-                    console.log("Liked: ", url, "\n");
-                }else{
-                    console.log(err);
-                }
-            });
+        //Like or follow logic here
+        if(likeParam){
+            for(let i=0; i<data.statuses.length; i++){
+                let id = {id: data.statuses[i].id_str};
+                T.post('favorites/create', id, (err, response)=>{
+                    if(!err){
+                        let userName = response.user.screen_name;
+                        let id = response.id_str;
+                        let url = `https://twitter.com/${userName}/status/${id}`;
+    
+                        console.log("Liked: ", url);
+                    }else{
+                        console.log(err);
+                    }
+                });
+            }
+        }else{
+            for(let i=0; i<data.statuses.length; i++){
+                // console.log(data.statuses[i]);
+                let screen_name = data.statuses[i].user.screen_name;
+                T.post('friendships/create', {screen_name}, (err, response)=>{
+                    if(!err){
+                        console.log(`Followed: ${screen_name}`);
+                    }else{
+                        console.log(err);
+                    }
+                });
+            }
         }
+        
     }else{
         console.log("error:", err);
     }
